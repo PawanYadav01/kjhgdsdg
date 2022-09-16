e655ec1949dee719f2268bfca3dbded20b4cf03a
PawanYadav01 <0201csai105@niet.co.in>
2022-09-16 14:51:08 +0530

    first commit

---
+++ Kl.java
@@ -0,0 +1,6 @@
+public class Kl{
+    public static void main(String[] args) {
+        int n = 9;
+        System.out.println(n);
+    }
+}
---
+++ Klop.java
@@ -0,0 +1,6 @@
+public class Klop {
+    public static void main(String[] args) {
+        int n = 9;
+        System.out.println(n);
+    }
+}
